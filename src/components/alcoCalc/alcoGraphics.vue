1050f5b8016f9e02912652954e91f9d4628599e3
Slava Mokeiev <slavamokeev666@gmail.com>
2019-04-14 21:35:54 +0300

    alcoGraf

--- src/components/alcoCalc/alcoGraphics.vue
+++ src/components/alcoCalc/alcoGraphics.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="small">
     <line-chart :chart-data="datacollection"></line-chart>
-    <button @click="fillData()">Randomize</button>
+    <v-btn @click="fillData">Посторить график</v-btn>
+    <!-- <div v-for="(user, i) in allUsers" :key="i">
+      <span @click="catchUser(user)">{{user}}</span> 
+    </div> -->
+    <v-layout wrap align-center>
+      <v-flex xs12 sm6 d-flex>
+        <v-select
+          v-model="currentUser"
+          @change="getUser"
+          :items="allUsers"
+          label="Users"
+        ></v-select>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -9,36 +22,80 @@
   import LineChart from '@/components/lineChart.js'
 
   export default {
+    data () {
+      return {
+        datacollection: {},
+        currentUser: null
+      }
+    },
     components: {
       LineChart
     },
-    data () {
-      return {
-        datacollection: {}
-      }
-    },
     mounted () {
-      this.fillData()
+      this.$store.dispatch('fetchUsers')
+      this.$store.dispatch('fetchAlcoUser')
     },
     methods: {
       fillData () {
         this.datacollection = {
-          labels: [this.getRandomInt(), this.getRandomInt()],
+          labels: [],
           datasets: [
             {
-              label: 'Data One',
-              backgroundColor: '#2EFE64',
-              data: [this.getRandomInt(), this.getRandomInt()]
-            }, {
-              label: 'Data One',
-              backgroundColor: '#2EFEF7',
-              data: [this.getRandomInt(), this.getRandomInt()]
+              label: 'Промиль',
+              borderColor: '#FC2525',
+              pointBackgroundColor: 'white',
+              borderWidth: 2,
+              pointBorderColor: 'white',
+              backgroundColor: '#79020285',
+              data: []
+            },
+            {
+              label: 'Промиль1',
+              borderColor: '#27ff02',
+              pointBackgroundColor: 'white',
+              borderWidth: 2,
+              pointBorderColor: 'white',
+              backgroundColor: '#27ff029c',
+              data: []
             }
           ]
         }
+        const promile = []
+        const date = []
+      
+        this.alcoGraphics.forEach((key) => {
+          promile.push(key.alcohol)
+        });
+        this.alcoGraphics.forEach((key) => {
+          date.push(`${new Date(key.date).getDate()}.${new Date(key.date).getMonth() + 1}`)
+        });
+        this.datacollection.datasets[0].data = promile.reverse()
+        this.datacollection.labels = date.reverse()
+        
+        const promile1 = []
+        if(this.currentAlcoUser === null || this.currentAlcoUser === undefined) {
+          promile1.push({})
+        }else {
+            Object.values(this.currentAlcoUser).forEach((key) => {
+            promile1.push(key)
+          })
+        } 
+        
+        this.datacollection.datasets[1].data = promile1.reverse()
       },
-      getRandomInt () {
-        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      getUser () {
+        this.$store.dispatch('currentAlcoUser', this.currentUser)
+      }
+    },
+    computed: {
+      alcoGraphics () {
+        return this.$store.getters.alcoGraphics
+      },
+      allUsers () {
+        return this.$store.getters.allUsers
+      },
+      currentAlcoUser () {
+        return this.$store.getters.currentAlcoUser
       }
     }
   }
--- src/store/modules/alcoCalc.js
+++ src/store/modules/alcoCalc.js
@@ -2,11 +2,19 @@ import * as fb from 'firebase'
 
 export default {
   state: {
-    alcoGraphics: []
+    alcoGraphics: [],
+    allUsers: [],
+    currentAlcoUser: null
   },
   mutations: {
     ALCO_USER(state, payload) {
       state.alcoGraphics = payload
+    },
+    FETCH_ALL_USER(state, payload) {
+      state.allUsers = payload
+    },
+    COMPARE_USER_ALCO(state, payload) {
+      state.currentAlcoUser = payload
     }
   },
   actions: {
@@ -31,14 +39,12 @@ export default {
         const currentUser = this.state.user.user.uid
         const allAlcoUser = await fb.database().ref(`users/${currentUser}/alcoholsUser`).once('value')
         const resultAlcoUser = []
-        if(allAlcoUser.val() !== null) {
-          Object.keys(allAlcoUser.val()).forEach((key)  => {
-            resultAlcoUser.push({
-              date: key,
-              alcohol: allAlcoUser.val()[key]
-            })
+        Object.keys(allAlcoUser.val()).forEach((key)  => {
+          resultAlcoUser.push({
+            date: key,
+            alcohol: allAlcoUser.val()[key]
           })
-        }      
+        })   
         commit('ALCO_USER', resultAlcoUser)
         commit('setLoading', false)
       } catch (error) {
@@ -47,10 +53,60 @@ export default {
         throw error
       }
       
+    },
+    async fetchUsers({commit}) {
+      commit('clearError')
+      commit('setLoading', true)
+
+      try {
+        const allUsers = await fb.database().ref(`users`).once('value')
+        
+        const fetchAllUsers = []
+        Object.keys(allUsers.val()).forEach((key) => {
+          fetchAllUsers.push(allUsers.val()[key].email)
+        })
+        commit('FETCH_ALL_USER', fetchAllUsers)
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
+        throw error
+      }
+    },
+    async currentAlcoUser({commit}, payload) {
+      commit('clearError')
+      commit('setLoading', true)
+
+      try {
+        const allUsers = await fb.database().ref(`users`).once('value')
+        let currentAlcoUser = {}
+        Object.keys(allUsers.val()).forEach((key) => {
+          if(allUsers.val()[key].email === payload) {
+              currentAlcoUser = allUsers.val()[key].alcoholsUser
+              if(currentAlcoUser === undefined) {
+                currentAlcoUser = {}
+              }
+          }
+        })
+        commit('COMPARE_USER_ALCO', currentAlcoUser)
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
+        throw error
+      }
     }
   },
   getters: {
-    
+    alcoGraphics (state) {
+      return state.alcoGraphics
+    },
+    allUsers (state) {
+      return state.allUsers
+    },
+    currentAlcoUser (state) {
+      return state.currentAlcoUser
+    }
   }
 }
     
